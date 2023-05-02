5fc300aca322e5ef243125d041d646bd93c0a726
Ruhanitamanna <tamannanasrin01@gmail.com>
2023-05-03 03:07:56 +0600

    footer created

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  FaCopyright,
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaWhatsapp,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-white p-4">
+      <div className="d-flex  justify-content-between">
+        <ul>
+          <li>FAQ</li>
+          <li>Feedback</li>
+          <li>Blog</li>
+          <li>Contact us</li>
+        </ul>
+        <div>
+          <p>
+            Recipes you want to make. Cooking advice that works.
+            <br /> Restaurant recommendations you trust.
+          </p>
+          <div className="d-flex justify-content-around py-4">
+            <FaWhatsapp />
+            <FaFacebook />
+            <FaInstagram />
+            <FaTwitter />
+          </div>
+        </div>
+      </div>
+      <p className="text-center">
+        <small>
+          <FaCopyright />
+          All Rights Reserved
+        </small>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import Main from "./layout/Main.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
