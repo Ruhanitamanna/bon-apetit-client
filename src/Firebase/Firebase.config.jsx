6c096d43e839df5adba268734b494ab81ae06430
Ruhanitamanna <tamannanasrin01@gmail.com>
2023-05-03 04:16:03 +0600

    installed firebase

---
+++ src/Firebase/Firebase.config.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-wWyfZIjV5rtQQezvY243HQvVx6gNFy4",
+  authDomain: "bon-appetit-assignment-10.firebaseapp.com",
+  projectId: "bon-appetit-assignment-10",
+  storageBucket: "bon-appetit-assignment-10.appspot.com",
+  messagingSenderId: "239132953456",
+  appId: "1:239132953456:web:eaa34ef2ee56bcdcd65db5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
